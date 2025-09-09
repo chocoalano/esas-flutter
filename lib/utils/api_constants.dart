15104ca4ae69881c73f2a344a473a5954fcb0735
chocoalano <alangentina95@gmail.com>
2025-09-09 09:49:15 +0700

    new update api

--- lib/utils/api_constants.dart
+++ lib/utils/api_constants.dart
@@ -1,6 +1,6 @@
-// const String baseApiUrl = 'http://128.199.111.239:8000/api';
+const String baseApiUrl = 'http://128.199.111.239:8000/api';
 // const String baseApiUrl = 'https://apiv2.sinergiabadisentosa.com/api';
-const String baseApiUrl = 'http://172.16.1.16:8000/api'; //ini wifi kantor
+// const String baseApiUrl = 'http://172.16.1.16:8000/api'; //ini wifi kantor
 // const String baseApiUrl = 'http://192.168.246.186:8000/api'; //ini hostpot hp
 const String baseImageUrl =
     'https://sas-assets.sgp1.cdn.digitaloceanspaces.com';
